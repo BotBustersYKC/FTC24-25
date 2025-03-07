620051f5be499b37e8017ace0c8bf28e1c40174e
IT Board <botbusters.ykc@gmail.com>
2025-03-07 13:23:36 +0100

    standard servo debug only

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug_2.java
@@ -1,40 +1,54 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Read_pos-debug", group="Robot")
+@TeleOp(name="Readposdebug", group="Robot")
 public class debug_2 extends LinearOpMode
 {
-    public Servo   wrist       = null; //the wrist servo
-    public Servo   folding     = null; //folding servo
-    public Servo   rotate       = null; // rotating servo arm
+    public DcMotor extendMotor  = null;
+    public Servo   Servo1       = null; //the wrist servo
+    public Servo   Servo2       = null; //folding servo
+    double Servo1_pos;
+    double Servo2_pos;
+    double extend_pos = 0;
 
-    double wrist_pos = 0;
-    double fold = 0;
-    double rotate_pos = 0;
-
-
+    final double EXTEND_TICKS_PER_DEGREE =
+            28 //encoder ticks
+                    *250047.0/4913.0 // exact ratio
+                    *1/360.0; // ticks per degree
 
     @Override
 
     public void runOpMode()
     {
-        wrist  = hardwareMap.get(Servo.class, "wrist"); //
-        folding = hardwareMap.get(Servo.class, "folding"); //
-        rotate = hardwareMap.get(Servo.class, "rotate"); //
+        Servo1  = hardwareMap.get(Servo.class, "Servo1"); //
+        Servo2 = hardwareMap.get(Servo.class, "Servo2"); //
+        extendMotor = hardwareMap.get(DcMotor.class, "extender");
+
+        extendMotor.setTargetPosition(0);
+        extendMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        extendMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
         while (opModeIsActive())
         {
 
-            wrist_pos = wrist.getPosition();
-            fold = folding.getPosition();
-            rotate_pos = rotate.getPosition();
+            Servo1_pos = Servo1.getPosition();
+            Servo2_pos = Servo2.getPosition();
 
-            telemetry.addData("Wrist Power", wrist_pos);
-            telemetry.addData("Fold position", fold);
-            telemetry.addData("Rotate position", rotate_pos);
+            if (gamepad1.a)
+            {
+                extend_pos = 2000*EXTEND_TICKS_PER_DEGREE;
+            }
+            if (gamepad1.b)
+            {
+                extend_pos = 0;
+            }
+
+            telemetry.addData("Servo1 pos", Servo1_pos);
+            telemetry.addData("Servo2 pos", Servo2_pos);
 
             telemetry.update();
         }
