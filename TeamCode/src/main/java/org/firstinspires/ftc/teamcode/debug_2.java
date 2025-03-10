52da41f26368d6a32bec3cdbfa4d52a751d3b9ff
IT Board <botbusters.ykc@gmail.com>
2025-03-10 13:12:28 +0100

    prep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug_2.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,7 +11,9 @@ public class debug_2 extends LinearOpMode
 {
     public Servo   Servo1       = null; //the wrist servo
     public Servo   Servo2       = null; //folding servo
+    public CRServo Servo3 = null;
     public DcMotor extendMotor = null;
+    public DcMotor armMotor = null;
     double Servo1_pos;
     double Servo2_pos;
     double position;
@@ -26,28 +29,47 @@ public class debug_2 extends LinearOpMode
     {
         Servo1  = hardwareMap.get(Servo.class, "Servo1"); //
         Servo2 = hardwareMap.get(Servo.class, "Servo2"); //
+        Servo3 = hardwareMap.get(CRServo.class, "Servo3");
         extendMotor = hardwareMap.get(DcMotor.class, "extender");
+        armMotor = hardwareMap.get(DcMotor.class, "left_arm");
 
         Servo2.setDirection(Servo.Direction.REVERSE);
 
         waitForStart();
         while (opModeIsActive())
         {
-            Servo1.setPosition(0);
-            Servo2.setPosition(0);
+            Servo1.setPosition(position);
+            Servo2.setPosition(position);
 
+            if (gamepad1.x)
+            {
+                position = .27;
+            }
+            if (gamepad1.y)
+            {
+                position = 0.1;
+            }
             if (gamepad1.a)
             {
-                extend = 2000*EXTEND_TICKS_PER_DEGREE;
+                Servo3.setPower(1);
             }
             if (gamepad1.b)
             {
-                extend = 0;
+                Servo3.setPower(-1);
+            }
+            if (gamepad1.dpad_up)
+            {
+                Servo3.setPower(0);
             }
 
-            extendMotor.setTargetPosition((int) (extend));
+
+            extendMotor.setTargetPosition(0);
             ((DcMotorEx) extendMotor).setVelocity(2100);
             extendMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            armMotor.setTargetPosition(0);
+            ((DcMotorEx) armMotor).setVelocity(2100);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             telemetry.addData("Servo1 pos", Servo1_pos);
             telemetry.addData("Servo2 pos", Servo2_pos);
