3d4d8576c4cc46fa192fc9cbf54e0f8d4dd428a9
IT Board <botbusters.ykc@gmail.com>
2025-03-11 11:42:05 +0100

    prep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug_2.java
@@ -11,12 +11,14 @@ public class debug_2 extends LinearOpMode
 {
     public Servo   Servo1       = null; //the wrist servo
     public Servo   Servo2       = null; //folding servo
-    public CRServo Servo3 = null;
+    public  Servo Servo3 = null;
+    public CRServo Servo4 = null;
     public DcMotor extendMotor = null;
     public DcMotor armMotor = null;
     double Servo1_pos;
     double Servo2_pos;
     double position;
+    double servo_3_pos;
     double extend;
     final double EXTEND_TICKS_PER_DEGREE =
             28 //encoder ticks
@@ -29,11 +31,13 @@ public class debug_2 extends LinearOpMode
     {
         Servo1  = hardwareMap.get(Servo.class, "Servo1");
         Servo2 = hardwareMap.get(Servo.class, "Servo2");
-        Servo3 = hardwareMap.get(CRServo.class, "Servo3");
+        Servo3 = hardwareMap.get(Servo.class, "Servo3");
+        Servo4 = hardwareMap.get(CRServo.class, "Servo4");
         extendMotor = hardwareMap.get(DcMotor.class, "extender");
         armMotor = hardwareMap.get(DcMotor.class, "left_arm");
 
         Servo2.setDirection(Servo.Direction.REVERSE);
+        //Servo4.setDirection(CRServo.Direction.REVERSE);
 
         waitForStart();
         while (opModeIsActive())
@@ -51,6 +55,26 @@ public class debug_2 extends LinearOpMode
             }
 
 
+            Servo3.setPosition(servo_3_pos);
+            if (gamepad1.dpad_up)
+            {
+                servo_3_pos += .005;
+            }
+            if (gamepad1.dpad_down)
+            {
+                servo_3_pos -= .005;
+            }
+
+            if (gamepad1.dpad_left)
+            {
+                Servo4.setPower(1);
+            }
+            if (gamepad1.dpad_right)
+            {
+                Servo4.setPower(0);
+            }
+
+
             extendMotor.setTargetPosition(0);
             ((DcMotorEx) extendMotor).setVelocity(2100);
             extendMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -60,6 +84,7 @@ public class debug_2 extends LinearOpMode
             armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             telemetry.addData("Position:", position);
+            telemetry.addData("Rotating Servo:", servo_3_pos);
 
             telemetry.update();
         }
